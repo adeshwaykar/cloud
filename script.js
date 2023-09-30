be364391ac6255724c0e242a21f1e000bf2ae608
adeshwaykar <128480380+adeshwaykar@users.noreply.github.com>
2023-09-30 23:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("hide").style.display = 'none';
+document.getElementById("buttonHide").style.display ='none';
+
+
+function allServicess(){
+    document.getElementById("buttonHide").style.display ='';
+    document.getElementById("hide").style.display = '';
+    document.getElementById("buttonHide").style.display ='';
+    document.getElementById("allServicess").style.display ='none';
+
+}
+
+function hideServices(){
+    document.getElementById("hide").style.display = 'none';
+document.getElementById("buttonHide").style.display ='none';
+document.getElementById("allServicess").style.display ='';
+
+}
